7a4fcfab984e64a94d5eea50d8991f8d902ee00f
mohammedanas <mohammedanas121.ks@gmail.com>
2022-07-11 01:32:08 -0700

    section 2 completed

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Entities
+{
+    public class AppUser
+    {
+        public int Id{set; get;}
+
+        public string UserName{set;get;}
+    }
+}
