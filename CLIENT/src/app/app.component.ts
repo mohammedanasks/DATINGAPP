8634d92c64a378f40e8b09415760e01ca5cde448
mohammedanas <mohammedanas121.ks@gmail.com>
2022-07-11 19:51:45 -0700

    section 3 angular started

---
+++ CLIENT/src/app/app.component.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+  title = 'CLIENT';
+  users : any;
+
+  constructor(private http : HttpClient){
+
+  } 
+
+  GetUser(){
+    this.http.get("https://localhost:7210/api/Users").subscribe(respons=>{
+      this.users=respons
+    },error =>{
+      console.log(error)
+    });
+    
+
+  }
+  
+  ngOnInit(){
+    this.GetUser()
+  }
+  
+}
+
+
